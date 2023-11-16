77f6947b4ac827b027f44512248415e8c65747ab
LeTommyyy <letommyyy@gmail.com>
2023-11-15 19:43:16 -0800

    basic server start

---
+++ backend/server.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const app = express();
+const PORT = process.env.PORT || 3000;
+const rootRoutes = require("./routes/root");
+
+app.use("/", rootRoutes);
+
+app.listen(PORT, () => {
+    console.log(`Server started on port ${PORT}`);
+});
